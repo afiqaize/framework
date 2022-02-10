5c4cab5c562bf6079f9977ee611e51b5c8539752
Afiq Anuar <afiqaize@gmail.com>
2022-02-10 20:33:33 +0100

    sifter empty draft

---
+++ plugins/misc/sifter.h
@@ -0,0 +1,12 @@
+// -*- C++ -*-
+// author: afiq anuar
+// short: an interface for sifting through multiple triggers based on tiers
+// note: events may appear in both double and single muon datasets, and this code allows defining some priority ordering
+// note: such that if double muon is considered the higher priority one, then single muon events are accepted only if it's not a double muon event
+
+#ifndef FWK_SIFTER_H
+#define FWK_SIFTER_H
+
+
+
+#endif
