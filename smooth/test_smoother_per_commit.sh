7f994939dce6a13dd8c8f7fb2a158eaa9c7ce4ef
Afiq Anuar <afiqaize@gmail.com>
2022-04-02 01:01:57 +0200

    minor update

--- smooth/test_smoother_per_commit.sh
+++ smooth/test_smoother_per_commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ftag="$1"
-commonopt=('--tree' 'Events' '--file' '/nfs/dust/cms/group/exotica-desy/HeavyHiggs/Trees_mtt_B_new2/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8.root' '--variable' 'mtt : 320, 360, 400, 440, 480, 520, 560, 600, 640, 680, 720, 760, 800, 845, 890, 935, 985, 1050, 1140, 1300, 1700' '--variable' 'cHel = negate(b1k * b2k) + negate(b1r * b2r) + negate(b1n * b2n) : 5; -1, 1' '--weight' 'weight' '--bandwidth 0.2' '--bandwidth' '0.2')
+commonopt=('--tree' 'Events' '--file' '/nfs/dust/cms/group/exotica-desy/HeavyHiggs/split_trees_UL18_v5/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_em.root' '--variable' 'mtt : 320, 360, 400, 440, 480, 520, 560, 600, 640, 680, 720, 760, 800, 845, 890, 935, 985, 1050, 1140, 1300, 1700' '--variable' 'cHel = negate(b1k * b2k) + negate(b1r * b2r) + negate(b1n * b2n) : 5; -1, 1' '--weight' 'weight' '--bandwidth 0.2' '--bandwidth' '0.2')
 
 if [ -z ${ftag} ]; then
     ftag='dev'
@@ -9,7 +9,7 @@ else
     git checkout "${ftag}"
 fi
 
-make -B smoother && ./smoother "${commonopt[@]}" --type weight --mode smooth --systematic TT_uF --weight 'wgtu = weight * MEfac_up' --weight 'wgtd = weight * MEfac_down' --output oweight_${ftag}.root && ./smoother "${commonopt[@]}" --type tree --mode smooth --systematic Jer --output otree_${ftag}.root && ./smoother "${commonopt[@]}" --mode histogram --output ohistogram_${ftag}.root
+make -B smoother && ./smoother "${commonopt[@]}" --type weight --mode smooth --systematic QCDscale_MEFac_TT --weight 'wgtu = weight * MEfac_up' --weight 'wgtd = weight * MEfac_down' --output oweight_${ftag}.root && ./smoother "${commonopt[@]}" --type tree --mode smooth --systematic CMS_JER_13TeV --output otree_${ftag}.root && ./smoother "${commonopt[@]}" --mode histogram --output ohistogram_${ftag}.root
 roothcat ohistogram_${ftag}.root mtt_cHel_unroll | grep sum > sum_histogram_${ftag}
 
 for ifile in oweight_${ftag}.root otree_${ftag}.root; do
