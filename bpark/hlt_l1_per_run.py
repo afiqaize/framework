00e58659c15963a81e0d87cd2754e782a8db26ab
Afiq Anuar <afiqaize@gmail.com>
2022-04-18 22:55:37 +0200

    incorporate update made for the standard dileptonic prescale json

--- bpark/hlt_l1_per_run.py
+++ bpark/hlt_l1_per_run.py
@@ -1,6 +1,18 @@
 #! /bin/env python
 # python hlt_l1_per_run.py --runs run1,run2... --datasets dataset1,dataset2... <--keep/--ignore path1,path2... --output_name output>
-# python hlt_l1_per_run.py --runs $(cat gold_json_hlt_2018 | grep -v '#' | awk '{print $1}' | tr '\n' ',') --datasets ParkingBPH1,ParkingBPH2,ParkingBPH3,ParkingBPH4,ParkingBPH5,ParkingBPH6
+# python hlt_l1_per_run.py --runs $(cat gold_json_hlt_2018 | grep -v '#' | awk '{print $1}' | sort -u | tr '\n' ',') --datasets ParkingBPH1,ParkingBPH2,ParkingBPH3,ParkingBPH4,ParkingBPH5,ParkingBPH6
+
+# cmssw setup cmd
+# cmsrel CMSSW_11_2_5
+# cmsenv
+# scram b -j 12
+
+# pickle runs out:
+# cat Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON_pre.txt | tr ':' '\n' | tr ']' '\n' | tr '{' '\n' | tr ',' '\n' | tr ' ' '\n' | grep -F '"' | tr -d '"' > runs_2016_pre
+
+# python hlt_l1_per_run.py --runs $(cat runs_2016_pre | grep -v '#' | awk '{print $1}' | sort -u | tr '\n' ',') --datasets SingleElectron,SingleMuon,DoubleMuon,DoubleEG,MuonEG --output_name hlt_l1_2016_pre.json --ignore BTag,Tau,PFHT,PFMET,PFJet
+
+# note: out of date, due to hltConfigFromDB update, but preserved for history
 
 import os
 import json
@@ -27,12 +39,11 @@ def validate_environment():
         host_name = os.getenv("HOSTNAME")
         if "lxplus" not in host_name:
                 raise RuntimeError('Code relies on hltConfigFromDB, which needs to be ran at lxplus!')
-        min_version = (10, 6, 2)
+        min_version = (11, 2, 5)
         for v1, v2 in zip(get_cmssw_version(), min_version):
-                if v1 > v2:
-                        return True
                 if v1 < v2:
                         return False
+                return True
 
 parser = ArgumentParser()
 parser.add_argument('--runs', help = 'runs to consider, comma separated', required = True)
@@ -43,7 +54,7 @@ parser.add_argument('--ignore', help = 'ignore the following HLT paths, comma se
 args = parser.parse_args()
 
 if not validate_environment():
-        raise RuntimeError('Code relies on hltConfigFromDB, which needs to be ran from CMSSW > 10_6_2!!')
+        raise RuntimeError('Code relies on hltConfigFromDB, which needs to be ran from CMSSW > 11_2_5!!')
 
 runs = args.runs.split(',')
 datasets = args.datasets.split(',')
@@ -83,21 +94,20 @@ for run in runs:
                                 modules = getattr(process, path).moduleNames()
                                 for module in modules:
                                         module_attr = getattr(process, module)
-                                        if module_attr.type_() is 'HLTL1TSeed':
+                                        if module_attr.type_() == 'HLTL1TSeed':
                                                 seeds = module_attr.L1SeedsLogicalExpression.configValue()
                                                 seeds = seeds.replace("'", '')
                                                 seeds = seeds.split(' OR ')
                                                 if not path in hlt_l1:
                                                         hlt_l1[path] = seeds
                                                 else:
-                                                        if set(seeds) != set(hlt_l1[path]):
-                                                                print 'Stored seed list:'
-                                                                print hlt_l1[path]
-                                                                print 'New seed list:'
-                                                                print seeds
-                                                                raise RuntimeError('Path {} has conflicting L1 seed lists!'.format(path))
-        syscall('rm hlttmp.py')
-        syscall('rm hlttmp.pyc')
+                                                        s0 = set(hlt_l1[path])
+                                                        s1 = set(seeds)
+                                                        hlt_l1[path] = list(s0.union(s1))
+
+                                                # not that nice, but shenanigans like p.l1module1 + cms.ignore(p.l1module2) is a damn pain
+                                                break
+        syscall('rm hlttmp.py*')
 
 with open(args.output_name, 'w') as out:
         json.dump(collections.OrderedDict(sorted(hlt_l1.items())), out, indent = 2)
